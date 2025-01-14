90e74c14cc01a5b59be0be14821d7df7283addf0
alia <aliayousuf.izzat@gmail.com>
2025-01-14 15:27:08 +0500

    Add more init

--- src/components/provider.tsx
+++ src/components/provider.tsx
@@ -9,8 +9,8 @@ export default function CartProvider({ children }: { children: ReactNode }) {
       mode="payment"
       cartMode="client-only"
       stripe={process.env.NEXT_PUBLIC_STRIPE_KEY as string}
-      successUrl="http://localhost:3000/stripe/success"
-      cancelUrl="http://localhost:3000/stripe/error"
+      successUrl="https://milestone3-ecommerce-mu.vercel.app/stripe/success"
+      cancelUrl="https://milestone3-ecommerce-mu.vercel.app/stripe/error"
       currency="USD"
       billingAddressCollection={false}
       shouldPersist={true}
